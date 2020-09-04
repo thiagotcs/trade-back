10fed9e995714b55975a7ff0578c675270265422
Thiago Carvalho <thiagotcs.dev@gmail.com>
2020-09-04 20:18:10 -0300

    remove user route do bootstrap

--- server.js
+++ server.js
@@ -40,7 +40,6 @@ app.get('/', (req, res) => {
 
 // routes
 require('./app/routes/auth.routes')(app);
-require('./app/routes/user.routes')(app);
 require('./app/routes/address.routes')(app);
 
 // set port, listen for requests
